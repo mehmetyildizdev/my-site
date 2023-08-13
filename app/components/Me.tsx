e293853f42ac1ad2d69d8e7c67d6a15835e39f2b
Mehmet YILDIZ <memostar91@gmail.com>
2023-08-13 23:58:28 +0300

    Content and layout edited

--- app/components/Me.tsx
+++ app/components/Me.tsx
@@ -21,86 +21,66 @@ function Me() {
   return (
     <>
       <Center h={`calc(100vh - 4em)`}>
-        <SimpleGrid columns={{ base: 1, md: 3, lg: 5 }} width={1080} p={8}>
-          <Box
+        <SimpleGrid columns={{ base: 1, md: 5 }} width={1080} p={8}>
+          <SimpleGrid
             gridColumn={{
               base: "1 / span 1",
-              md: "1 / span 1",
-              lg: "1 / span 1",
+              md: "1 / span 4",
             }}
-            borderColor="p.gold"
-            borderRadius="24"
-            boxShadow={`0 8px 16px 0px ${shadowColor}`}
+            columns={{ base: 1, md: 4 }}
           >
-            <Center className="p-4">
-              <Avatar size="2xl" name="Mehmet YILDIZ" src="/1.jpg" />
-            </Center>
-            <Center className="pt-2">
-              <Box>
-                <Flex className="pt-2">
-                  <BiSolidUser />{" "}
-                  <Heading as="h5" size="xs">
-                    Mehmet YILDIZ
-                  </Heading>
-                </Flex>
-
-                <Flex className="pt-2">
-                  <BiSolidBusiness />
-                  <Heading as="h5" size="xs">
-                    Fake Scientist & IT Guy
-                  </Heading>
-                </Flex>
-                <Flex className="pt-2">
-                  <BiSolidMap />
-                  <Heading as="h5" size="xs">
-                    Based in Turkey
-                  </Heading>
-                </Flex>
-              </Box>
-            </Center>
-            <Box position="relative" width="100%" height={height}>
-              <Image
-                className="opacity-50 rounded-3xl pt-8"
-                src="/ankara-pin.jpg"
-                alt=""
-                layout="fill"
-                objectFit="cover"
-              />
+            <Box
+              gridColumn={{
+                base: "1 / span 1",
+                md: "1 / span 1",
+              }}
+              borderColor="p.gold"
+              borderRadius="24"
+              boxShadow={`0 8px 16px 0px ${shadowColor}`}
+            >
+              <Center className="p-4">
+                <Avatar size="2xl" name="Mehmet YILDIZ" src="/1.jpg" />
+              </Center>
             </Box>
-          </Box>
-          <Box
+            <Box
+              gridColumn={{
+                base: "1 / span 1",
+                md: "2 / span 3",
+              }}
+              p={{ base: "2", md: "8" }}
+              pt={{ base: "8", md: "4" }}
+            >
+              <Text textAlign="justify">
+                I am a full-stack web developer and designer with a passion for
+                computer technologies and a college degree in web design and
+                programming. I have a diverse background in history and
+                academia, which enhances my research and problem-solving
+                abilities.
+              </Text>
+              <br />
+              <Text textAlign="justify">
+                I currently work as an IT Specialist at a manufacturing company,
+                where I handle various software and e-commerce platforms, as
+                well as company websites and network devices. I am always eager
+                to learn new skills and explore new challenges. I am looking for
+                opportunities to grow and advance in my field and create useful
+                and innovative applications.
+              </Text>
+            </Box>
+          </SimpleGrid>
+          <SimpleGrid
             gridColumn={{
               base: "1 / span 1",
-              md: "2 / span 2",
-              lg: "2 / span 2",
+              md: "5 / span 1",
             }}
-            p={{ base: "2", md: "8" }}
-            pt={{ base: "8", md: "4" }}
           >
-            <Text textAlign="justify">
-              I am currently working as Information Technologies Specialist in
-              Wenta Heat Technologies. Main responsibilities are managing ERP
-              software, Alibaba and other local e-commerce platforms, company
-              websites, and network devices.
-            </Text>
-            <br />
-            <Text textAlign="justify">
-              I am an academic career dropout. This helps me to be open to
-              research and learn whenever needed. Being passionate about
-              computer technologies eventually directed me to this field.
-            </Text>
-          </Box>
-          <Box
-            gridColumn={{
-              base: "1 / span 1",
-              md: "1 / span 3",
-              lg: "4 / span 2",
-            }}
-            p={{ base: "2", md: "4" }}
-            pt={{ base: "8", md: "8", lg: "4" }}
-          >
-            Right Column
-          </Box>
+            <Box
+              p={{ base: "2", md: "4" }}
+              pt={{ base: "8", md: "8", lg: "4" }}
+            >
+              Right Column
+            </Box>
+          </SimpleGrid>
         </SimpleGrid>
       </Center>
     </>
